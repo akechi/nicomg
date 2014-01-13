fdf3bbdd4c77e6fda382aff0a144e374e0e714dd
raa0121 <raa0121@gmail.com>
2014-01-13 20:52:12 +0900

    fixfix

--- nicomg.rb
+++ nicomg.rb
@@ -7,7 +7,7 @@ agent.ssl_version = 'SSLv3'
 secure_url = 'https://secure.nicovideo.jp/secure/login?site=niconico'
 rss = agent.get("http://seiga.nicovideo.jp/rss/manga/#{ARGV[0]}")
 html = REXML::Document.new rss.body
-items = html.elements['rss/channel/item/link']
+items = html.elements.each('rss/channel/item/link') do |e| e.text end
 urls = items.map{|i|i.text}.sort
 agent.post(secure_url, 'mail' => config['account']['email'], 'password' => config['account']['passwd'])
 urls.map{|e|
@@ -17,7 +17,7 @@ urls.map{|e|
   FileUtils.mkdir_p(mg) unless FileTest.exist?(mg)
   manga = agent.get("http://seiga.nicovideo.jp/api/theme/data?theme_id=#{mg_num}")
   html = REXML::Document.new manga.body
-  images = html.elements['response/image_list/image/source_url']
+  images = html.elements.each('response/image_list/image/source_url') do |e| e.text end
   images_url = images.map{|i|i.text.sub('l?','p?')}.sort
   images_url.each_with_index do |item,i| agent.get(item).save_as("./#{mg.to_s}/#{i+1}.jpg") end
 }
